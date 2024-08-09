0f5f20ba715582212293ef3b4eda04bd7b0e8bb4
Mariano Scaglia Rat <mscagliarat@yahoo.com.ar>
2024-08-09 20:26:20 -0300

    dsilva index and login

--- disilva/urls.py
+++ disilva/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('myapp/', include('myapp.urls')),  # Ruta para la aplicación interna
+    path('accounts/', include('django.contrib.auth.urls')),
+    #path('myapp/', include('myapp.urls')),  # Ruta para la aplicación interna
+    path('', views.index, name='landingindex'),
 ]
