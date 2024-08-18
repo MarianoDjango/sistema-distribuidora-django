9d5949f56501cd640e4ca3de8f13e9a2f91b7418
Mariano Scaglia Rat <mscagliarat@yahoo.com.ar>
2024-08-17 23:35:21 -0300

    Lista navegacion dashboard

--- myapp/views.py
+++ myapp/views.py
@@ -17,7 +17,7 @@ def famlias_empresa(request, **kwargs):
     recs = []
     json_list = {}
     for fam in familias_var:
-        fila = '<a href="javascript:void(0);" class="list-group-item border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar" onclick="ponerfamilia(' +  str(fam.id) + ' , ' +  fam.nombre + ')"><i class="bi bi-bootstrap"></i><span>' + fam.nombre + '</span></a>'
+        fila = f'<a href="javascript:void(0);" class="list-group-item border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar" onclick="ponerfamilia({fam.id}, \'{fam.nombre}\')"><span>{fam.nombre}</span></a>'
         json_list = {
             'fila': fila,
         }
@@ -26,25 +26,34 @@ def famlias_empresa(request, **kwargs):
     return HttpResponse(data, 'application/json')
 
 def articulos_famila(request, **kwargs):
-    familia_id = request.GET['familia']
-    nombre_var = request.GET['nombre']
-    if nombre_var == "":
-        articles = articulos.objects.filter(familia=int(familia_id)).values('descripcion', 'precio_venta', 'stock', 'familia')
-    else:
-        articles = articulos.objects.filter(familia=int(familia_id), descripcion__icontains=nombre_var).values('descripcion', 'precio_venta', 'stock')
     recs = []
     json_list = {}
-    familia = familias.objects.get(id=int(familia_id))
-    recs.append(json_list)
-    for articulo in articles:
-        fila = '<tr style="cursor:hand;">'
-        fila += '<td>' + articulo['descripcion'] + '</td>'
-        fila += '<td>' + str(articulo['precio_venta']) + '</td>'
-        fila += '<td>' + str(articulo['stock']) + '</td>'
-        json_list = {
-            'fila': fila,
-        }
+    if request.user.is_authenticated:
+        familia_id = request.GET['familia']
+        nombre_var = request.GET['nombre']
+        idempresa_var = request.GET['idempresa']
+        if nombre_var == "":
+            articles = articulos.objects.filter(familia=int(familia_id), activo=True).values('idempresa','descripcion', 'precio_venta', 'fecha_precio', 'stock', 'fecha_stock', 'familia' )
+        else:
+            articles = articulos.objects.filter(familia=int(familia_id), descripcion__icontains=nombre_var, activo=True).values('descripcion', 'precio_venta', 'fecha_precio', 'stock', 'fecha_stock')
+        familia = familias.objects.get(id=int(familia_id))
         recs.append(json_list)
+        for articulo in articles:
+            fila = '<tr style="cursor:hand;">'
+            fila += '<td><a href="#">' + articulo['descripcion'] + '</a></td>'
+            fila += '<td class="text-end"><a href="#">' + str(articulo['precio_venta']) + '</a></td>'
+            fila += '<td class="text-center">' + str(articulo['fecha_precio']) + '</td>'
+            fila += '<td class="text-end"><a href="#">' + str(articulo['stock']) + '</a></td>'
+            fila += '<td class="text-center">' + str(articulo['fecha_stock']) + '</td>'
+            if str(articulo['idempresa']) == idempresa_var:
+                fila += f'<td class="text-center"><input type="text" class="form-control" style="border-radius: 10px;" placeholder="Cant."></td>'
+                fila += f'<td class="text-center"><button class="btn" style="background-color: #92dea3;"><i class="bi bi-cart-plus"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16"><path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9z"/><path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zm3.915 10L3.102 4h10.796l-1.313 7zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/></svg></i></button>''</td>'
+                fila += f'<td class="text-center"><div class="form-check"><input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"><label class="form-check-label" for="flexCheckDefault"></label></div></td>'
+
+            json_list = {
+                'fila': fila,
+            }
+            recs.append(json_list)
     
     data = json.dumps(recs)
     return HttpResponse(data, 'application/json')
@@ -58,7 +67,8 @@ class dashboard_view(View, LoginRequiredMixin):
             empresa_user = self.request.user.perfil.idempresa.id
             familias_var = familias.objects.filter(idempresa=empresa_user)
             familia = familias_var[0]
-            context = {'empresas': companies_var, 'id_empresa':int(empresa_user), 'id_familia' : familia.id, 'familia_nom' : familia.nombre }
+            empresa = str(companies_var[0]).split((" - ")[1])
+            context = {'empresas': companies_var, 'id_empresa':int(empresa_user),'nomempresa' : empresa[1],  'id_familia' : familia.id, 'familia_nom' : familia.nombre }
 
         return render(self.request, 'myapp/dashboard.html', context)
 
