3f4925789b345b33933b0d2c218a702348a6090d
Mariano Scaglia Rat <mscagliarat@yahoo.com.ar>
2024-09-05 20:52:56 -0300

    Practicamente finalizado

--- myapp/urls.py
+++ myapp/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('articulo/<id_empresa>/<pk>/', views.articulo_create_or_update, name='article-detail'),
     path('actualizar_precios/', views.actualizar_precios, name='updateprices'),
     path('guardar_movimiento/', views.guardar_movs_stock, name='updatestocks'),
+    path('dashboard/<id_empresa>/agregar/<int:articulo_id>/', views.agregar_al_carrito, name='agregar_al_carrito'),
+    path('dashboard/<id_empresa>/carrito/', views.ver_carrito, name='ver_carrito'),
+
 ]
